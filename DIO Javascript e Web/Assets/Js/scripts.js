8cf45eed4a2573cad3aeece9ad4e58e075813c63
Felipe <felipe.efb2003@gmail.com>
2022-06-22 02:06:53 -0300

    projeto do Javascript até o momento

---
+++ DIO Javascript e Web/Assets/Js/scripts.js
@@ -0,0 +1,28 @@
+var buttonLess =  document.getElementsByTagName("subtrair");
+var buttonPlus =  document.getElementsByTagName("adicionar");
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber= 0 ;
+
+function decrement(){
+
+    if( currentNumber>=0){
+        document.getElementById("currentNumber").style.color = "red";
+    }
+    if(currentNumber >= -4){
+        currentNumber = currentNumber - 1 ;
+        buttonLess.disabled= false;
+    } else {
+        buttonLess.disabled= true;
+    }
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function increment(){
+    if(currentNumber<= 19){
+        currentNumber = currentNumber + 1 ;
+        buttonPlus.disabled= false;
+    } else {
+        buttonPlus.disabled= true;
+    }
+    currentNumberWrapper.innerHTML = currentNumber;
+}
